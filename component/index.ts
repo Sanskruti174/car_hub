e0e564e4c90e2f34cb56d562f5ab1c60811c4517
urjitasawant <141426807+urjitasawant@users.noreply.github.com>
2023-11-25 15:48:06 +0530

    update index

--- component/index.ts
+++ component/index.ts
@@ -1,6 +1,11 @@
 import hero from './hero';
+import CustomButton from './CustomButton';
+import Navbar from './Navbar';
+import Footer from './Footer';
 
 export{
     hero,
     CustomButton,
+    Navbar,
+    Footer,
 }
